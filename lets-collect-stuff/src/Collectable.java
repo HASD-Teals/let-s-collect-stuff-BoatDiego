61d25df3c038bdd6b539173e3899e2ff79c9b5a5
BoatNasa25 <Diegovera2511@gmail.com>
2022-02-24 10:05:11 -0600

    djhdbkd

--- lets-collect-stuff/src/Collectable.java
+++ lets-collect-stuff/src/Collectable.java
@@ -1,3 +1,4 @@
+
 public class Collectable {
     // Add collectable properties here
     private int susLevel;
@@ -5,10 +6,12 @@ public class Collectable {
     private String Color;
     private boolean imposter;
     private boolean thicc;
-    private boolean vent;   
+    private boolean vent;
     private int bodies;
+    private double dripLevel;
+
     // Add collectable constructors here
-    public Collectable(){
+    public Collectable() {
         this.susLevel = 0;
         this.Name = "";
         this.Color = "";
@@ -16,9 +19,21 @@ public class Collectable {
         this.thicc = true;
         this.vent = false;
         this.bodies = 0;
+        this.dripLevel = 0;
     }
-    public Collectable(int susLevel, String Name, String Color, boolean imposter, boolean thicc, boolean vent, 
-    int bodies){
+/**
+ * Parameters for collectable
+ * @param susLevel
+ * @param Name
+ * @param Color
+ * @param imposter
+ * @param thicc
+ * @param vent
+ * @param bodies
+ * @param dripLevel
+ */
+    public Collectable(int susLevel, String Name, String Color, boolean imposter, boolean thicc, boolean vent,
+            int bodies, double dripLevel) {
         this.susLevel = susLevel;
         this.Name = Name;
         this.Color = Color;
@@ -26,9 +41,121 @@ public class Collectable {
         this.thicc = thicc;
         this.vent = vent;
         this.bodies = bodies;
+        this.dripLevel = dripLevel;
     }
-    
-    // Add collectable accessors and mutators here
+
+    // accessors
+    public int getSusLevel() {
+        return this.susLevel;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public String getColor() {
+        return this.Color;
+    }
+
+    public boolean getImposter() {
+        return this.imposter;
+    }
+
+    public boolean getThicc() {
+        return this.thicc;
+    }
+
+    public boolean getVent() {
+        return this.vent;
+    }
+
+    public int getBodies() {
+        return this.bodies;
+    }
+
+    public double dripLevel() {
+        return this.dripLevel;
+    }
+
+    // mutators
+    public void setSusLevel(int susLevel) {
+        this.susLevel = susLevel;
+    }
+
+    public void setBodies(int bodies) {
+        this.bodies = bodies;
+    }
+
+    public void setDripLevel(double dripLevel) {
+        this.dripLevel = dripLevel;
+    }
+
+    public void setImposter(boolean imposter) {
+        this.imposter = imposter;
+    }
+
+    public void setThicc(boolean thicc) {
+        this.thicc = thicc;
+    }
+
+    public void setVent(boolean vent) {
+        this.vent = vent;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
 
     // Add any methods here
+    public boolean isEqual(Collectable com) {
+        boolean equal = true;
+        if (this.Name.equalsIgnoreCase(com.getColor())) {
+            System.out.println("They have the same colors with " + this.Color + " " + com.Color);
+        } else {
+            System.out.println("Not same colors");
+            equal = false;
+        }
+        if (this.imposter == com.getImposter()) {
+
+            System.out.println("Both are imposters with " + this.imposter + " " + com.imposter);
+        } else {
+            System.out.println("both are not imposters");
+            equal = false;
+        }
+        if (this.thicc == com.getThicc()) {
+
+            System.out.println("Both are very Thicc " + this.thicc + " " + com.thicc);
+        } else {
+            System.out.println("Not thicc enough for each other");
+            equal = false;
+        }
+        if (this.vent == com.getVent()) {
+
+            System.out.println("They both vented " + this.vent + " " + com.vent);
+        } else {
+            System.out.println("one vented here, one did not");
+            equal = false;
+        }
+        if (this.susLevel == com.susLevel) {
+            System.out.println("Both are sus with " + this.susLevel + " " + com.susLevel);
+        } else {
+            System.out.println("Not same sus level");
+            equal = false;
+        }
+        if (this.dripLevel == com.dripLevel) {
+            System.out.println("Both are drippy af equally with " + this.dripLevel + " " + com.dripLevel);
+        } else {
+            System.out.println("Not same drip level, someone more dripped out");
+            equal = false;
+        }
+        return equal;
+    }
+
+
+
+
 }
--- lets-collect-stuff/src/CollectionClient.java
+++ lets-collect-stuff/src/CollectionClient.java
@@ -1,8 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionClient {
     public static void main(String[] args) throws Exception {
         // Add your collection ArrayList here..
+        List<Collectable> Amogus  = new ArrayList<>(); 
+        Collectable redAmogus= new Collectable(10, "Sussy Baka", "red", true, true, true, 5, 10);  );
+        Amogus.add(redAmogus);
+
         // e.g. List<Collectable> collection = new ArrayList<Collectable>();
 
         // Add collection methods here to manage and manipulate the collection
+    }
+
+    public static int countItems(ArrayList<Collectable> Amogus) {
+        int nums = Amogus.size();
+
+        return nums;
+    }
+
+    public static double totalDrip(ArrayList<Collectable> Amogus) {
+        int drip = 0;
+        for (int i = 0; i < Amogus.size(); i++) {
+            Collectable temps = Amogus.get(i);
+            drip += temps.dripLevel();
+        }
+
+        return drip;
+    }
+
+    public static Collectable display(int i, ArrayList<Collectable> Amogus) {
+        Collectable temps = Amogus.get(i);
+
+        return temps;
+    }
+
+    public static Collectable display(String name, ArrayList<Collectable> Amogus) {
+        Collectable fin = null;
+        for (int i = 0; i < Amogus.size(); i++) {
+            Collectable temps = Amogus.get(i);
+            if (name.equalsIgnoreCase(temps.getName())) {
+                fin = temps;
+            } else {
+                System.out.println("Could not find AMOGUS by name");
+            }
+
+        }
+        return fin;
+    }
+
+    public static void displayAll(ArrayList<Collectable> Amogus) {
+        for (int i = 0; i < Amogus.size(); i++) {
+            Collectable tempy = Amogus.get(i);
+            System.out.print("Number " + i + 1 +
+                    "\nName: " + tempy.getName() +
+                    "\nColor: " + tempy.getColor() +
+                    "\nSus Level: " + tempy.getSusLevel() +
+                    "\nis Imposter: " + tempy.getImposter() +
+                    "\nIs Thicc:" + tempy.getThicc() +
+                    "\nvented: " + tempy.getVent() +
+                    "\nbodies: " + tempy.getBodies() +
+                    "\nDRIP LEVEL: " + tempy.dripLevel());
+            System.out.println("\n\n");
+
+        }
+
     }
 }
